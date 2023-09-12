f578eadafdf764454c6e463d6a91dd51a4003913
ShaMaxDev <maksimbychkov@yahoo.com>
2023-09-11 22:22:03 -0700

    Add project files.

---
+++ Task Hospital/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_Hospital
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int hour = 60;
+            int timePerPerson = 10;
+            int receptionLine;
+            Console.WriteLine("How many people in reception line?");
+            receptionLine = Convert.ToInt32(Console.ReadLine());
+            int waitingHours = (receptionLine * timePerPerson) / hour;
+            int waitingMinutes = (receptionLine * timePerPerson) % hour;
+            Console.WriteLine("You will wait " + waitingHours + " hours and " + waitingMinutes + " minutes");
+        }
+    }
+}
